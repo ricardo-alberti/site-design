e29139aca0ecb3033a7fb5d6438e3a07fac1ea6a
Ricardo Alberti <ricardo.alberti.26@gmail.com>
2022-05-16 17:32:24 -0300

    separa o javascript do index.html

---
+++ script.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    var x = document.getElementById("items2");
+    var y = document.getElementById("nav2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+fetch("imoveis.json")
+    .then( response => response.json()) 
+    .then( data => {
+    for(var i = 0; i < data.imoveis.length; i++)
+    { 
+        document.querySelector("#local"+i).innerText = data.imoveis[i].local
+        document.querySelector("#adress"+i).innerText = data.imoveis[i].adress
+        document.querySelector("#img"+i).src = data.imoveis[i].img
+        document.querySelector("#price"+i).innerText = data.imoveis[i].price
+        document.querySelector("#sell"+i).innerText = data.imoveis[i].sell
+        document.querySelector("#link"+i).href = data.imoveis[i].link
+        document.querySelector("#area"+i).innerText = data.imoveis[i].area
+        document.querySelector("#bedroons"+i).innerText = data.imoveis[i].bedroons
+        document.querySelector("#bathroons"+i).innerText = data.imoveis[i].bathroons
+        document.querySelector("#garages"+i).innerText = data.imoveis[i].garages
+        console.log(data) 
+    }
+})
+
