34f292c03574a16b565c81e8fc13eecf1c4f8d24
Ricardo Alberti <ricardo.alberti.26@gmail.com>
2022-07-07 23:22:47 -0300

    search bar logic

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function search() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('div');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
 function myFunction() {
     var x = document.getElementById("items2");
     var y = document.getElementById("nav2");
